01031d48e9eb2f28feb778f7467b2fde2bb2d1cb
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-26 11:00:11 -0800

    completed challenge 1

--- Frames-Bounds-Center.playground/Contents.swift
+++ Frames-Bounds-Center.playground/Contents.swift
@@ -2,4 +2,9 @@
 
 import UIKit
 
-
+let superFrame = CGRect(x: 0, y: 0, width: 750, height: 1334)
+let superView = UIView(frame: superFrame)
+superView.backgroundColor = .white
+let redView = UIView(frame: superView.bounds.insetBy(dx: 20, dy: 20))
+redView.backgroundColor = .red
+superView.addSubview(redView)
