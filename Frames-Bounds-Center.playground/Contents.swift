416219cc71c212ca0abc5e2694fa0fec0ceed3f7
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-26 11:08:40 -0800

    built core functionality for challenge 2

--- Frames-Bounds-Center.playground/Contents.swift
+++ Frames-Bounds-Center.playground/Contents.swift
@@ -8,3 +8,16 @@ superView.backgroundColor = .white
 let redView = UIView(frame: superView.bounds.insetBy(dx: 20, dy: 20))
 redView.backgroundColor = .red
 superView.addSubview(redView)
+let superView2 = UIView(frame: superFrame)
+superView2.backgroundColor = .white
+
+for i in 0..<8 {
+    let checkFrame = CGRect(x:CGFloat((superFrame.width/8)*CGFloat(i)), y:0, width: superFrame.width/8, height: superFrame.width/8)
+    let checkView = UIView(frame: checkFrame)
+    if (i%2) == 0 {
+        checkView.backgroundColor = .black
+    } else {
+        checkView.backgroundColor = .white
+    }
+    superView2.addSubview(checkView)
+}
