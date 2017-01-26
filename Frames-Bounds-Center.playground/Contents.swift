1e119d9d2706ad54e02e40d27341d4250a8b74eb
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-01-26 11:13:36 -0800

    built x and y functionality to generate checkeredView, just need to fix colors

--- Frames-Bounds-Center.playground/Contents.swift
+++ Frames-Bounds-Center.playground/Contents.swift
@@ -11,13 +11,18 @@ superView.addSubview(redView)
 let superView2 = UIView(frame: superFrame)
 superView2.backgroundColor = .white
 
-for i in 0..<8 {
-    let checkFrame = CGRect(x:CGFloat((superFrame.width/8)*CGFloat(i)), y:0, width: superFrame.width/8, height: superFrame.width/8)
-    let checkView = UIView(frame: checkFrame)
-    if (i%2) == 0 {
-        checkView.backgroundColor = .black
-    } else {
-        checkView.backgroundColor = .white
+for j in 0..<8 {
+    let rowFrame = CGRect(x: 0, y:CGFloat((superFrame.width/8)*CGFloat(j)), width: superFrame.width, height: superFrame.width/8)
+    let rowView = UIView(frame: rowFrame)
+    for i in 0..<8 {
+        let checkFrame = CGRect(x:CGFloat((superFrame.width/8)*CGFloat(i)), y:0, width: superFrame.width/8, height: superFrame.width/8)
+        let checkView = UIView(frame: checkFrame)
+        if (i%2) == 0 {
+            checkView.backgroundColor = .black
+        } else {
+            checkView.backgroundColor = .white
+        }
+        rowView.addSubview(checkView)
     }
-    superView2.addSubview(checkView)
+    superView2.addSubview(rowView)
 }
